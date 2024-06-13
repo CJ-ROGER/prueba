55fd017764d5ccef965af5458fe9d196a5eb1453
Franklin Roger Castañeda Jimenez <you@example.com>
2024-06-12 23:08:19 -0500

    Operacion Multiplicasion

--- r2.py
+++ r2.py
@@ -1,2 +1,12 @@
+# Que miras bobo :v
 
-print("Aprendiendo a subir archivos a GitHub")
+print("\t\t**Menu de Multiplicasion*")
+num1 = int(input("\nIngrese un numero: "))
+num2 = int(input("\nIngrese un numero: "))
+
+
+multiplicasion= num1*num2
+
+print(f"\nLa Multiplicasion de los numeros es: {multiplicasion} ")
+
+
